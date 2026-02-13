f0d587a056d063a2696766d14b69349b817694bb
Shankar9952 <gowrisankarraoseera@gmail.com>
2026-02-13 23:36:21 +0530

    Add files via upload
    latest chgs

---
+++ script.js
@@ -0,0 +1,47 @@
+window.onload = function () {
+
+  const yesBtn = document.getElementById("yes");
+  const noBtn = document.getElementById("no");
+  const title = document.querySelector("#message-box h1");
+  const messageBox = document.getElementById("message-box");
+  const question = document.getElementById("question");
+  const buttons = document.getElementById("buttons");
+
+  yesBtn.onclick = function () {
+
+  title.innerText = "Happy Valentines Day Lavanya-Sanchana❤️ I love you so much!";
+
+  question.style.display = "none";
+  buttons.style.display = "none";
+
+  // Change background image
+  document.body.style.background = "url('')";
+  document.body.style.backgroundSize = "cover";
+  document.body.style.backgroundPosition = "center";
+
+  setInterval(createHeart, 300);
+};
+
+  noBtn.onmouseover = function () {
+    noBtn.style.position = "absolute";
+    noBtn.style.left = Math.random() * 80 + "%";
+    noBtn.style.top = Math.random() * 80 + "%";
+  };
+
+  function createHeart() {
+    const heart = document.createElement("div");
+    heart.className = "heart";
+    heart.innerHTML = "❤️";
+
+    heart.style.left = Math.random() * 100 + "vw";
+
+    document.body.appendChild(heart);
+
+    setTimeout(() => {
+      heart.remove();
+    }, 4000);
+  }
+
+};
+
+
